f3a706dcbceb5ad373ffb3697dc9750d390aecaf
Jesse WB <JesseWB.C@gmail.com>
2018-05-23 15:10:51 -0400

    Updated to extract one column from the website

--- HDI Table Scraping.R
+++ HDI Table Scraping.R
@@ -1,11 +1,18 @@
-library(rvest)
+library('revest')
 
+#loop through all possible pages
+for(i in 1:2) {
+  
+  #Read html page
+  HDI_Calendar_page <- paste("https://www.thinkhdi.com/education/calendar.aspx?pg=", i, sep="") %>%
+    read_html()
+  
+  #get vector of course names from page
+  HDI_Calendar_CourseNames
+  HDI_Calendar_CourseNames <- HDI_Calendar_page %>%
+    html_nodes(".CourseSchedule-CourseLink") %>%
+    html_text() %>%
+    append(HDI_Calendar_CourseNames)
 
-
-
-for(i in 1:20) {
- 
-  url <- paste(https://www.thinkhdi.com/education/calendar.aspx?pg=, i, sep="")
-  web_page <- read_html
-   
 }
+
