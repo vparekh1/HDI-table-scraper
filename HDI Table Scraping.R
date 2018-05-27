7073def9f78640ba59b0082cda49ed059ebfdb36
Jesse WB <JesseWB.C@gmail.com>
2018-05-27 00:22:01 -0400

    The scraper can now get the course titles from the table

--- HDI Table Scraping.R
+++ HDI Table Scraping.R
@@ -1,18 +1,26 @@
-library('revest')
+library('rvest')
+library('magrittr')
+
+HDI_Calendar_CourseNames <- character()
+HDI_Calendar_Locations <- character()
 
 #loop through all possible pages
 for(i in 1:2) {
   
   #Read html page
-  HDI_Calendar_page <- paste("https://www.thinkhdi.com/education/calendar.aspx?pg=", i, sep="") %>%
+  HDI_Calendar_Page <- paste("https://www.thinkhdi.com/education/calendar.aspx?pg=", i, sep="") %>%
     read_html()
   
   #get vector of course names from page
-  HDI_Calendar_CourseNames
-  HDI_Calendar_CourseNames <- HDI_Calendar_page %>%
+  HDI_Calendar_CourseNames <- HDI_Calendar_Page %>%
     html_nodes(".CourseSchedule-CourseLink") %>%
     html_text() %>%
     append(HDI_Calendar_CourseNames)
+  
+  #get vector of locations
+  HDI_Calendar_Locations <- HDI_Calendar_Page %>%
+    html_nodes("br+span") %>%
+    html_text() %>%
+    append(HDI_Calendar_Locations)
 
 }
-
