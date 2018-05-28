17b7c5e6a9aac7f33190790c339941e5b58f23ee
Jesse WB <JesseWB.C@gmail.com>
2018-05-28 10:01:45 -0600

    Now correctly parses the start and end dates. Doesn't parse locations.

--- HDI Table Scraping.R
+++ HDI Table Scraping.R
@@ -3,6 +3,8 @@ library('magrittr')
 
 HDI_Calendar_CourseNames <- character()
 HDI_Calendar_Locations <- character()
+HDI_Calendar_Dates <- character()
+
 
 #loop through all possible pages
 for(i in 1:2) {
@@ -19,8 +21,17 @@ for(i in 1:2) {
   
   #get vector of locations
   HDI_Calendar_Locations <- HDI_Calendar_Page %>%
-    html_nodes(".CourseSchedule-lightbox-location") %>%
+    html_nodes(".CourseSchedule table tr .CourseList-Item span") %>%
     html_text() %>%
     append(HDI_Calendar_Locations, .)
-
+  
+  #get vector of dates.
+  HDI_Calendar_Dates <- HDI_Calendar_Page %>%
+    html_nodes(".CourseSchedule-lightbox-dates span") %>%
+    html_text() %>%
+    append(HDI_Calendar_Dates, .)
 }
+
+#Split dates into start and end
+HDI_Calendar_Dates_Start <- HDI_Calendar_Dates[c(TRUE, FALSE)]
+HDI_Calendar_Dates_END <- HDI_Calendar_Dates[c(FALSE, TRUE)]
