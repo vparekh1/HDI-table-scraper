82eb60e2bd782b3a0c5c3e9e95e3e94f910f6a26
Jesse WB <JesseWB.C@gmail.com>
2018-05-28 23:32:33 -0600

    Now scrapes all the relevant info. Can be made more concise with less calls to the web page. Need to add data frame and factors.

--- HDI Table Scraping.R
+++ HDI Table Scraping.R
@@ -19,19 +19,25 @@ for(i in 1:2) {
     html_text() %>%
     append(HDI_Calendar_CourseNames, .)
   
-  #get vector of locations
-  HDI_Calendar_Locations <- HDI_Calendar_Page %>%
-    html_nodes(".CourseSchedule table tr .CourseList-Item span") %>%
-    html_text() %>%
-    append(HDI_Calendar_Locations, .)
   
   #get vector of dates.
   HDI_Calendar_Dates <- HDI_Calendar_Page %>%
     html_nodes(".CourseSchedule-lightbox-dates span") %>%
     html_text() %>%
     append(HDI_Calendar_Dates, .)
+  
+  #get vector of locations
+  HDI_Calendar_Locations <- HDI_Calendar_Page %>%
+    html_nodes(".CourseList-Item:not(.CourseSchedule-Register) > span  ") %>%
+    html_text() %>%
+    append(HDI_Calendar_Locations, .)
+  
 }
 
 #Split dates into start and end
 HDI_Calendar_Dates_Start <- HDI_Calendar_Dates[c(TRUE, FALSE)]
 HDI_Calendar_Dates_END <- HDI_Calendar_Dates[c(FALSE, TRUE)]
+
+#Get locations from matrix
+HDI_Calendar_City <- HDI_Calendar_Locations[c(FALSE, FALSE, TRUE, FALSE)]
+HDI_Calendar_State <- HDI_Calendar_Locations[c(FALSE, FALSE, FALSE, TRUE)]
