e4868d9edce577da2d67cbfad3a9d2882b263d28
Jesse WB <JesseWB.C@gmail.com>
2018-05-29 21:40:11 -0700

    The data is now factorized and stored in a data frame.

--- HDI Table Scraping.R
+++ HDI Table Scraping.R
@@ -2,8 +2,7 @@ library('rvest')
 library('magrittr')
 
 HDI_Calendar_CourseNames <- character()
-HDI_Calendar_Locations <- character()
-HDI_Calendar_Dates <- character()
+HDI_Calendar_Data <- character()
 
 
 #loop through all possible pages
@@ -20,24 +19,32 @@ for(i in 1:2) {
     append(HDI_Calendar_CourseNames, .)
   
   
-  #get vector of dates.
-  HDI_Calendar_Dates <- HDI_Calendar_Page %>%
-    html_nodes(".CourseSchedule-lightbox-dates span") %>%
-    html_text() %>%
-    append(HDI_Calendar_Dates, .)
+  # #get vector containing location and start and end dates
+  # HDI_Calendar_Dates <- HDI_Calendar_Page %>%
+  #   html_nodes(".CourseSchedule-lightbox-dates span") %>%
+  #   html_text() %>%
+  #   append(HDI_Calendar_Dates, .)
   
-  #get vector of locations
-  HDI_Calendar_Locations <- HDI_Calendar_Page %>%
+  #get vector containing location and start and end dates
+  #need the :not selector to avoid picking up the pop-up windows
+  HDI_Calendar_Data <- HDI_Calendar_Page %>%
     html_nodes(".CourseList-Item:not(.CourseSchedule-Register) > span  ") %>%
     html_text() %>%
-    append(HDI_Calendar_Locations, .)
-  
+    append(HDI_Calendar_Data, .)
+
 }
 
-#Split dates into start and end
-HDI_Calendar_Dates_Start <- HDI_Calendar_Dates[c(TRUE, FALSE)]
-HDI_Calendar_Dates_END <- HDI_Calendar_Dates[c(FALSE, TRUE)]
+#parse vector into start and end dates
+HDI_Calendar_Dates_Start <- HDI_Calendar_Data[c(TRUE, FALSE, FALSE, FALSE)]
+HDI_Calendar_Dates_END <- HDI_Calendar_Data[c(FALSE, TRUE, FALSE, FALSE)]
 
-#Get locations from matrix
-HDI_Calendar_City <- HDI_Calendar_Locations[c(FALSE, FALSE, TRUE, FALSE)]
-HDI_Calendar_State <- HDI_Calendar_Locations[c(FALSE, FALSE, FALSE, TRUE)]
+#parse vector into city and state
+HDI_Calendar_City <- HDI_Calendar_Data[c(FALSE, FALSE, TRUE, FALSE)]
+HDI_Calendar_State <- HDI_Calendar_Data[c(FALSE, FALSE, FALSE, TRUE)]
+
+#create data frame
+HDI_Calendar.df <- data.frame(Course_Name = HDI_Calendar_CourseNames,
+                              Start_Date = HDI_Calendar_Dates_Start, 
+                              End_Date = HDI_Calendar_Dates_END, 
+                              City = HDI_Calendar_City,
+                              State = HDI_Calendar_State)
